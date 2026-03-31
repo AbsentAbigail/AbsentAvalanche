7e1689050e2e525674d8398fb3d0a9dccf4670fa
Abigail <xxoliwerxx@gmail.com>
2026-03-31 15:45:51 +0200

    Fix Cuddles crashing Cuddle Pips

---
+++ Patches/StatusFixes/WhenAllyHealedRunApplyFix.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace AbsentAvalanche.Patches.StatusFixes;
+
+[HarmonyPatch(typeof(StatusEffectApplyXWhenAllyHealed), nameof(StatusEffectApplyXWhenAllyHealed.RunApplyStatusEvent), typeof(StatusEffectApply))]
+public class WhenAllyHealedRunApplyFix
+{
+    [UsedImplicitly]
+    private static bool Prefix(ref bool __result, StatusEffectApplyXWhenAllyHealed __instance, StatusEffectApply apply)
+    {
+        if (apply.effectData is null || apply.target is null)
+        {
+            __result = false;
+            return false;
+        }
+        __result = __instance.target.enabled && apply.target != __instance.target && apply.target.owner == __instance.target.owner && apply.effectData.type == "max health up" && Battle.IsOnBoard(__instance.target) && Battle.IsOnBoard(apply.target);
+        return false;
+    }
+}
